fe18330ed32c788274f3bc6dd48f8ef3fc57f90a
vadik <vadik@WadZap.(none)>
2013-06-23 04:06:43 +0400

    new implementation of knapsack added

---
+++ knapsack_dp_01/knapsack_int.hpp
@@ -0,0 +1,12 @@
+#ifndef KNAPSACK_INT_H_
+#define KNAPSACK_INT_H_
+
+#define INPUT_BUFF_SIZE 256
+#define max(a,b) (a > b ? a : b)
+
+typedef struct {
+    int weight;
+    int value;
+} Knapsack_item;
+
+#endif
